bcb013fa91119a4f1a92f9132a0fbcde029da948
El-Efraa <efraingregorio3@gmail.com>
2023-08-03 01:55:07 -0300

    Cree la estructura de archivos del codigo

---
+++ Codigo/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve (__dirname, './public')
+app.use (express.static (publicPath))
+
+app.listen(3000, () =>{
+    console.log("Servidor corriendo en el puerto 3000")
+})
+
+app.get ('/', function(req, res){
+    res.sendFile (path.resolve (__dirname, './views/home.html'))
+})
+
+app.get ('/registro', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/registro.html'))
+})
+
+app.get ('/login', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/login.html'))
+})
+
+app.get ('/producto', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/producto.html'))
+})
+
+app.get ('/carrito', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/carrito.html'))
+})
